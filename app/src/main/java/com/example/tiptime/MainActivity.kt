74da577bb8124213f2eb319901766f7510a6a84e
Cheng-Ying Li <newtaiwanstar@gmail.com>
2022-10-02 07:07:28 +0800

    refactor: follow the hints

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -6,7 +6,7 @@ import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
-    lateinit var building: ActivityMainBinding
+    private lateinit var building: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,21 +18,25 @@ class MainActivity : AppCompatActivity() {
     private fun calculateTip() {
         val stringInTextField = building.costOfService.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
-        if (cost == null) {
-            building.tipResult.text = ""
+        // If the cost is null or 0, then display 0 tip and exit this function early.
+        if (cost == null || cost == 0.0) {
+            displayTip(0.0)
             return
         }
-        val selectedId = building.tipOptions.checkedRadioButtonId
-        val tipPercentage = when (selectedId) {
+        val tipPercentage = when (building.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else ->0.15
         }
         var tip = tipPercentage * cost
-        val roundUp = building.roundUpSwitch.isChecked
-        if (roundUp) {
+        if (building.roundUpSwitch.isChecked) {
             tip = kotlin.math.ceil(tip)
         }
+        // Display the formatted tip value on screen
+        displayTip(tip)
+    }
+
+    private fun displayTip(tip: Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         building.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
