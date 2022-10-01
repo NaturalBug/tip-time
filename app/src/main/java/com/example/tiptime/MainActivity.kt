e6eaa12b2a47d75e0963f39a933ebf24c9cb5db4
Cheng-Ying Li <newtaiwanstar@gmail.com>
2022-10-02 06:44:22 +0800

    feat: calculate tip

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.tiptime
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
     lateinit var building: ActivityMainBinding
@@ -11,5 +12,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         building = ActivityMainBinding.inflate(layoutInflater)
         setContentView(building.root)
+        building.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val stringInTextField = building.costOfService.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectedId = building.tipOptions.checkedRadioButtonId
+        val tipPercentage = when (selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else ->0.15
+        }
+        var tip = tipPercentage * cost
+        val roundUp = building.roundUpSwitch.isChecked
+        if (roundUp) {
+            tip = kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        building.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
