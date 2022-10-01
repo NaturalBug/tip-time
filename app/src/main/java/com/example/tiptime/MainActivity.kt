c2538f91e10bcb1a64b2072612fbc855aad3a889
Cheng-Ying Li <newtaiwanstar@gmail.com>
2022-10-02 07:10:22 +0800

    style: add empty lines

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -10,28 +10,34 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         building = ActivityMainBinding.inflate(layoutInflater)
         setContentView(building.root)
+
         building.calculateButton.setOnClickListener { calculateTip() }
     }
 
     private fun calculateTip() {
         val stringInTextField = building.costOfService.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
+
         // If the cost is null or 0, then display 0 tip and exit this function early.
         if (cost == null || cost == 0.0) {
             displayTip(0.0)
             return
         }
+
         val tipPercentage = when (building.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else ->0.15
         }
+
         var tip = tipPercentage * cost
         if (building.roundUpSwitch.isChecked) {
             tip = kotlin.math.ceil(tip)
         }
+
         // Display the formatted tip value on screen
         displayTip(tip)
     }
