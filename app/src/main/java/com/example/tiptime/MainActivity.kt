6da61c5e9afdffa2c8b006e10917af6f3df68319
Cheng-Ying Li <newtaiwanstar@gmail.com>
2022-10-01 18:28:09 +0800

    feat: use view building

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tiptime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var building: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        building = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(building.root)
     }
 }
