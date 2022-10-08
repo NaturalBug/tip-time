add648767714b5177cdb1dc9cfb9a0b6b59ca3a5
Cheng-Ying Li <newtaiwanstar@gmail.com>
2022-10-09 07:27:58 +0800

    test: add other integration tests

--- app/src/androidTest/java/com/example/tiptime/CalculatorTests.kt
+++ app/src/androidTest/java/com/example/tiptime/CalculatorTests.kt
@@ -7,15 +7,11 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.Matchers.containsString
-
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
-import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -35,5 +31,43 @@ class CalculatorTests {
             .perform(click())
         onView(withId(R.id.tip_result))
             .check(matches(withText(containsString("$10.00"))))
+    }
+
+    @Test
+    fun calculate_18_percent_tip() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50.00"))
+        onView(withId(R.id.option_eighteen_percent))
+            .perform(click())
+        onView(withId(R.id.calculate_button))
+            .perform(click())
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("$9.00"))))
+    }
+
+    @Test
+    fun calculate_15_percent_tip_round_up() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50.00"))
+        onView(withId(R.id.option_fifteen_percent))
+            .perform(click())
+        onView(withId(R.id.calculate_button))
+            .perform(click())
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("$8.00"))))
+    }
+
+    @Test
+    fun calculate_15_percent_tip_no_rounding() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50.00"))
+        onView(withId(R.id.option_fifteen_percent))
+            .perform(click())
+        onView(withId(R.id.round_up_switch))
+            .perform(click())
+        onView(withId(R.id.calculate_button))
+            .perform(click())
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("$7.50"))))
     }
 }
